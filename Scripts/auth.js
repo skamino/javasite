15f7b8a3b252620cc84de07d49ba8b0cea8240f0
skamino <kaminostephen@yahoo.ca>
2020-09-19 23:06:39 -0400

    added the switch

--- Scripts/auth.js
+++ Scripts/auth.js
@@ -38,7 +38,18 @@
                 event.preventDefault();
                 window.location.replace("https://skamino.github.io/javasite/index.html");
             });
-            let lesson1 = document.getElementById("lesson1");
+
+            let thumbs = document.getElementsByTagName("button");
+            for(const butt of thumbs){
+                butt.addEventListener("click", (event)=>{
+                    event.preventDefault();
+                    let but_id = butt.getAttribute("id");
+                    switch_function(but_id);
+                });
+            }
+
+
+           /* let lesson1 = document.getElementById("lesson1");
             lesson1.addEventListener("click", (event) =>{
                 event.preventDefault();
                 //call function to reload page
@@ -47,9 +58,54 @@
             src1.addEventListener("click", (event) =>{
                 event.preventDefault();
                 //call function to reload page
-            })
+            });*/
         })
     }
 
+    function switch_function(page){
+        document.title = page;
+        window.history.pushState("", "", page);
+
+        let XHR = new XMLHttpRequest();
+
+        let partial = "./Partial/" + page + ".html";
+        console.log(partial);
+
+        XHR.open("GET", parital);
+        XHR.send();
+        XHR.addEventListener("readystatechange", function(){
+            let main_body = document.getElementsByTagName("main")[0];
+            main_body.innerHTML = XHR.responseText;
+
+            let git_button = document.getElementById("toGit");
+            git_button.addEventListener("click", (event) =>{
+                event.preventDefault();
+                window.location.replace("https://github.com/skamino");   
+            });
+            let exit_button = document.getElementById("exit");
+            exit_button.addEventListener("click", (event) =>{
+                event.preventDefault();
+                window.location.replace("https://skamino.github.io/javasite/index.html");
+            });
+            let thumbs = document.getElementsByTagName("button");
+            for(const butt of thumbs){
+                butt.addEventListener("click", (event)=>{
+                    event.preventDefault();
+                    let but_id = butt.getAttribute("id");
+                    switch_function(but_id);
+                });
+            }
+        });
+        /*switch(page)
+        {
+            case "lesson1":
+
+                break;
+            case "src1":
+                break;
+            default:
+                break;
+        }*/
+    }
     window.addEventListener("load", Start, false);
 })();
