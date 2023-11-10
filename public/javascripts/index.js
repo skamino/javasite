6b4a3bc714dfb2f3a4476f014abea9117ea6177d
cursed1 <kaminostephen@yahoo.ca>
2023-11-10 12:46:56 -0500

    added scripts

---
+++ public/javascripts/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+(function() { 
+    function Start()
+    {
+        console.log("javascript loaded...");
+        
+    }
+   
+    window.addEventListener("load", Start, false);
+})();
