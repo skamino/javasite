1bbe7743546563d54dd560607b4ce5e66af8c91e
cursed1 <kaminostephen@yahoo.ca>
2023-11-10 12:43:15 -0500

    organizing file system

---
+++ public/javascripts/auth.js
@@ -0,0 +1,11 @@
+"use strict";
+
+(function() { 
+    function Start()
+    {
+        console.log("App Started...");
+        
+    }
+   
+    window.addEventListener("load", Start, false);
+})();
