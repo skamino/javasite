c88429ab277ae801dc2cd05eabd19dc83d61366f
cursed1 <kaminostephen@yahoo.ca>
2023-11-10 11:52:56 -0500

    just learning about how github pages works

--- server/config/db.js
+++
@@ -1,5 +0,0 @@
-module.exports = 
-{
-    "URI": "mongodb://localhost/javasite"
-    //"URI": "mongodb+srv://skamino:RuVGzNNtzp5d0lTt@comp229-a2.pqjcv.mongodb.net/MySite?retryWrites=true&w=majority"
-}
